8daabee73de35b8aa4381ffdc0f1fc2ba6a5b17a
Alexis <lxs.bnds@gmail.com>
2020-02-04 18:30:30 -0800

    project handlebar complete

--- server.js
+++ server.js
@@ -21,42 +21,56 @@ app.set("view engine", "handlebars");
 app.use(express.static("public"));
 
 
-
-//include project objects
+// cvf//include project objects
 const project1 = {
 name: "Election Year",
-technology: ["Node JS", "HTML", "Bootstrap"],
-repo: "link",
-description: "Get Real Detailed here"
+technology: ["Node JS ", " Charts JS ", "Bootstrap", ],
+deployed: true,
+live: "https://mycandidates.herokuapp.com/",
+repo: "https://github.com/boundsalexis/tinfoil-hat",
+description: "Get Real Detailed here",
+img:"/assets/political.png"
 };
 const project2 = {
 name: "FitMe",
-technology: ["Node JS", "HTML"],
-repo: "link"
+technology: ["Node JS  ", "HTML"],
+deployed: true,
+live:"https://skuttenkuler.github.io/FitME/",
+repo: "https://github.com/boundsalexis/FitME",
+img: "/assets/fitme.png"
 };
 const project3 = {
 name: "Note Taker",
-technology: ["Node JS", "HTML"],
-repo: "link",
-description: "Get Real Detailed here"
+technology: ["Node JS ", "HTML"],
+repo: "https://github.com/boundsalexis/note-taker",
+deployed: true,
+live:"https://mysterious-waters-03191.herokuapp.com/",
+description: "Get Real Detailed here",
+img:"/assets/notetaker.png"
 };
 const project4 = {
 name: "Weather Board",
-technology: ["Node JS", "HTML"],
-repo: "link",
-description: "Get Real Detailed here"
+technology: ["Node JS ", "HTML"],
+deployed: false,
+repo: "https://github.com/boundsalexis/Weather-Board",
+description: "Get Real Detailed here",
+img:"/assets/weatherboard.png"
 };
 const project5= {
 name: "Team Manager",
-technology: ["Node JS", "HTML"],
-repo: "link",
-description: "Get Real Detailed here"
+technology: ["Node JS ", "HTML"],
+deployed: false,
+repo: "https://github.com/boundsalexis/team-generator",
+description: "Get Real Detailed here",
+img:"/assets/teamgenerator.png"
 };
 const project6 = {
 name: "Content Management",
-technology: ["Node JS", "HTML"],
-repo: "link",
-description: "Get Real Detailed here"
+technology: ["Node JS ", "HTML"],
+deployed: false,
+repo: "https://github.com/boundsalexis/content-management-system",
+description: "Get Real Detailed here",
+img: "/assets/contentmanagemen.png"
 };
 
 // var routes = require("./routes/html-routes.js")(app);
@@ -76,14 +90,14 @@ app.get('/portfolio', function(req,res){
     res.render("portfolio");
 });
 
-app.get("*", function(req,res){
+app.get("/:anythingelse", function(req,res){
     res.render("index");
 });
 
 app.get('/portfolio/:project', function(req, res){
     console.log(req.params.project);
     switch(req.params.project){
-        case 'jesus':
+        case '1':
             res.render("project", project1);
             break;
         case '2':
@@ -96,10 +110,10 @@ app.get('/portfolio/:project', function(req, res){
             res.render("project", project4);
             break;
         case '5':
-            res.render("project", project4);
+            res.render("project", project5);
             break;
         case '6':
-            res.render("project", project4);
+            res.render("project", project6);
             break;   
     }
 });
